c19de1aa82ec8c64b5b3ea02a46708a756effb68
XDD44 <50290408+xdd44-utopia@users.noreply.github.com>
2025-04-26 17:09:39 -0400

    finish new slider

--- js/gallery.js
+++ js/gallery.js
@@ -2,78 +2,99 @@ var currentPage = 0;
 var num = 0;
 var lastScroll = 0;
 
+numIntroPage = 2;
 numPage = 6;
-barTotalHeight = 40;
-titleTexts = ['Title', 'Introduction', 'Visualization', 'Rationale & Critique', 'Development Process', 'Plan']
+var barLength = 100 / (numPage - numIntroPage);
+
+titleTexts = ['Visualization', 'Rationale & Critique', 'Development Process', 'Plan']
 
 
 document.onkeydown = onKeyDown;
 window.onload = setUp();
+addEventListener("scroll", (event) => {
+	checkScroll();
+});
+addEventListener("resize", (event) => {
+	checkScroll();
+});
 
 function setUp() {
-    num = document.getElementsByClassName('container').length;
-    document.getElementById('upArrow').onclick = scrollUp;
-    document.getElementById('downArrow').onclick = scrollDown;
-    setInterval(checkScroll, 20);
-    currentPage = 0;
+	num = document.getElementsByClassName('container').length;
+	document.getElementById('upArrow').onclick = scrollUp;
+	document.getElementById('downArrow').onclick = scrollDown;
+	currentPage = 0;
+	document.getElementById('currentTitle').style.height = `${barLength}%`;
+	checkScroll();
 }
 
 function onKeyDown(event) {
-    if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(event.code) > -1) {
-        event.preventDefault();
-    }
-    var event = event || window.event;
-    switch (event.keyCode) {
-        case 38:
-            scrollUp();
-            break;
-        case 87:
-            scrollUp();
-            break;
-        case 40:
-            scrollDown();
-            break;
-        case 83:
-            scrollDown();
-            break;
-    }
+	if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(event.code) > -1) {
+		event.preventDefault();
+	}
+	var event = event || window.event;
+	switch (event.keyCode) {
+		case 38:
+			scrollUp();
+			break;
+		case 87:
+			scrollUp();
+			break;
+		case 40:
+			scrollDown();
+			break;
+		case 83:
+			scrollDown();
+			break;
+	}
 }
 
 function scrollUp() {
-    if (currentPage > 0) {
-        currentPage--;
-        scroll(currentPage);
-    }
+	if (currentPage > 0) {
+		currentPage--;
+		scroll(currentPage);
+	}
 }
 
 function scrollDown() {
-    if (currentPage < num - 1) {
-        currentPage++;
-        scroll(currentPage);
-    }
+	if (currentPage < num - 1) {
+		currentPage++;
+		scroll(currentPage);
+	}
 }
 
 function scroll(index) {
-    document.getElementsByClassName('container')[index].scrollIntoView();
+	document.getElementsByClassName('container')[index].scrollIntoView();
 }
 
 function checkScroll() {
-    for (let i = 0; i < num; i++) {
-        if (document.getElementsByClassName('container')[i].getBoundingClientRect().y >= -10) {
-            currentPage = i;
-            break;
-        }
-    }
+	for (let i = 0; i < num; i++) {
+		const top = document.getElementsByClassName('container')[i].getBoundingClientRect().top;
+		const bottom = document.getElementsByClassName('container')[i].getBoundingClientRect().bottom;
+		const center = (top + bottom) / 2;
+		if (center > 5 && center < window.innerHeight - 5) {
+			currentPage = i;
+			break;
+		}
+	}
 
-    document.getElementById('currentTitleText').innerText = titleTexts[currentPage];
-    document.getElementById('upperBar').style.height = `${barTotalHeight / (numPage - 1) * currentPage}vh`;
-    document.getElementById('lowerBar').style.height = `${barTotalHeight / (numPage - 1) * (numPage - currentPage - 1)}vh`;
+	if (currentPage < numIntroPage) {
+		document.getElementById('progressBarContainer').style.left = "100vw";
+		document.getElementById('currentTitleText').innerText = "";
+		document.getElementById('upperBar').style.height = "0";
+		document.getElementById('lowerBar').style.height = "100%";
+	}
+	else {
+		document.getElementById('progressBarContainer').style.left = "98vw";
+		document.getElementById('currentTitleText').innerText = titleTexts[currentPage - numIntroPage];
+		document.getElementById('upperBar').style.height = `${(currentPage - numIntroPage) * barLength}%`;
+		document.getElementById('lowerBar').style.height = `${(numPage - numIntroPage - currentPage + 1) * barLength}%`;
+	}
 
-    document.getElementById('upArrowImg').style.display = "block";
-    document.getElementById('downArrowImg').style.display = "block";
-    if (currentPage == 0) {
-        document.getElementById('upArrowImg').style.display = "none";
-    } else if (currentPage == num - 1) {
-        document.getElementById('downArrowImg').style.display = "none";
-    }
+	document.getElementById('upArrowImg').style.display = "block";
+	document.getElementById('downArrowImg').style.display = "block";
+	if (currentPage == 0) {
+		document.getElementById('upArrowImg').style.display = "none";
+	} else if (currentPage == num - 1) {
+		document.getElementById('downArrowImg').style.display = "none";
+	}
 }
